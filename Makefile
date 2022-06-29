f3bae254ea1ae03aa05a7c5495554cab36a58e3e
Tikei-42 <tarek.r.eldana@gmail.com>
2022-06-29 18:02:47 -0400

    Added .vscode to ignore, added prototypes for functions and Make

---
+++ Makefile
@@ -0,0 +1,15 @@
+NAME	=	test
+
+CC		=	gcc
+CFLAGS	=	-Wall -Werror -Wextra
+RM 		=	rm -rf
+
+SRCS	=	$(wildcard *.c)
+OBJS	=	$(SRCS:.c=.o)
+
+# Test function used with main
+$(NAME): $(SRCS)
+	@clear
+	@$(CC) $(CFLAGS) -o $@ $^
+	@./test
+	@$(RM) $@
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tel-dana <tel-dana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 17:50:47 by tel-dana          #+#    #+#             */
+/*   Updated: 2022/06/29 17:54:35 by tel-dana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdbool.h>
+
+char	*get_next_line(int fd);
+
+#endif
