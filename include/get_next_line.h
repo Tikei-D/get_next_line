6df4be3530baa10e5eb739bc239d8474a8c69ea8
Tikei-42 <tarek.r.eldana@gmail.com>
2022-09-19 19:14:49 -0400

    Moved the project into new directory

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -1,15 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tel-dana <tel-dana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/29 17:50:54 by tel-dana          #+#    #+#             */
-/*   Updated: 2022/06/29 17:53:09 by tel-dana         ###   ########.fr       */
+/*   Created: 2022/06/28 15:42:02 by tel-dana          #+#    #+#             */
+/*   Updated: 2022/09/01 14:49:45 by tel-dana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
 
 char	*get_next_line(int fd);
+int		ft_strlen_gnl(const char *str);
+char	*ft_strdup_gnl(char *s1);
+char	*ft_strjoin_gnl(char *s1, char *s2);
+char	*ft_strchr_gnl(char *s, int c);
+void	*ft_calloc_gnl(int count, int size);
+char	*ft_substr_gnl(char *s, int start, int len);
+
+#endif
